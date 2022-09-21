93ead9dd30dcc25108f08971645ed2969ad0e02d
Karol Libardi <karolmajta@gmail.com>
2022-09-21 13:57:23 +0200

    Added /admin/best-clients

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -18,7 +18,6 @@ function adminRoutes(app) {
 
     const startDate = start.startOf("day").toDate();
     const endDate = end.endOf("day").toDate();
-    console.log(startDate, endDate);
 
     const result = await Job.findOne({
       raw: true,
@@ -53,6 +52,68 @@ function adminRoutes(app) {
     res.json({
       profession: result["Contract.Contractor.profession"],
     });
+  });
+
+  app.get("/admin/best-clients", async (req, res) => {
+    const { Job, Contract, Profile } = req.app.get("models");
+    const sequelize = req.app.get("sequelize");
+    const start = moment(req.query.start, "YYYY-MM-DD");
+    const end = moment(req.query.end, "YYYY-MM-DD");
+    const limit = parseInt(req.query.limit || "2", 10);
+
+    const paramsInvalid =
+      !start.isValid() ||
+      !end.isValid() ||
+      start.isAfter(end) ||
+      isNaN(limit) ||
+      limit < 0;
+
+    if (paramsInvalid) {
+      res.status(400).send();
+      return;
+    }
+
+    const startDate = start.startOf("day").toDate();
+    const endDate = end.endOf("day").toDate();
+
+    const result = await Job.findAll({
+      raw: true,
+      nest: true,
+      include: {
+        model: Contract,
+        required: true,
+        include: {
+          model: Profile,
+          required: true,
+          as: "Client",
+        },
+      },
+      where: {
+        paid: { [Op.eq]: true },
+        [Op.and]: [
+          { paymentDate: { [Op.gt]: startDate } },
+          { paymentDate: { [Op.lte]: endDate } },
+        ],
+      },
+      group: ["Contract.ClientId"],
+      attributes: [
+        [sequelize.fn("sum", sequelize.col("Job.price")), "totalSpent"],
+      ],
+      order: [["totalSpent", "desc"]],
+      limit: limit,
+    });
+
+    if (!result) {
+      res.status(404).send();
+      return;
+    }
+
+    res.json(
+      result.map((r) => ({
+        Client: r.Contract.Client,
+        totalSpent: r.totalSpent,
+      }))
+    );
   });
 
   return app;
